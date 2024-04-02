646dfc89f812e7bbeaa0d466716ad9d38e67e08e
omaalii <omarmoustafa0024@gmail.com>
2024-04-02 04:02:19 +0200

    70

--- fractol.h
+++ fractol.h
@@ -25,6 +25,27 @@ to make the rendering math easier*/
 #define WIDTH 800
 #define HEIGHT 800
 
+/* COLOURS
+*/
+#define BLACK   0x000000
+#define WHITE   0xFFFFFF
+#define RED     0xFF0000
+#define GREEN   0x00FF00
+#define BLUE    0x0000FF
+
+//Psychedelic Colours
+#define PSYCHEDELIC_LIME       0xBFFF00
+#define PSYCHEDELIC_CYAN_BLUE  0x00FFFF
+#define PSYCHEDELIC_MAGENTA    0xFF00FF
+#define PSYCHEDELIC_VIOLET     0xEE82EE
+#define PSYCHEDELIC_TANGERINE  0xFFA500
+#define PSYCHEDELIC_INDIGO     0x4B0082
+#define PSYCHEDELIC_TURQUOISE  0x40E0D0
+#define PSYCHEDELIC_GOLD       0xFFD700
+#define PSYCHEDELIC_CORAL      0xFF7F50
+#define PSYCHEDELIC_SPRING     0x00FF7F
+
+
 /*
  COMPLEX values
  */
@@ -62,6 +83,8 @@ typedef struct  s_fractal
     //Image
     t_img   img;
     //Hooks member variable
+    double  escape_value;//hypotenuse
+    int     iterations;//tied to the image quality
 }				t_fractal;                    
 
 // ***** String Utils *****
@@ -70,5 +93,10 @@ void	put_str_fd(char *s, int fd);
 
 //***** init *****
 void    fractal_init(t_fractal *fractal);
+
+//***** math utils *****
+double  map(double unscaled_num, double new_min, double new_max, double old_min, double old_max);
+t_complex   complex_sum(t_complex z1, t_complex z2);
+t_complex   complex_square(t_complex z);
 
 #endif
--- math_utils.c
+++ math_utils.c
@@ -16,3 +16,20 @@ double  map(double unscaled_num, double new_min, double new_max, double old_min,
 {
     return (new_max - new_min) * (unscaled_num - old_min) / (old_max - old_min) + new_min;
 }
+t_complex   complex_sum(t_complex z1, t_complex z2)
+{
+    t_complex   result;
+
+    result.x = z1.x + z2.x;
+    result.y = z1.y + z2.y;
+    return (result);
+}
+
+t_complex   complex_square(t_complex z)
+{
+    t_complex   result;
+
+    result.x = (z.x * z.x) - (z.y * z.y);
+    result.y = 2 * z.x * z.y;
+    return (result);
+}
